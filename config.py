2daf9b0598f7c4c31d3d5cc206ce4518813cc962
w88 <krippach147@gmail.com>
2025-09-04 19:49:22 +0500

    added start command and updated environment variables in config.py

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ load_dotenv()
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
+    IPMI_IP = os.getenv("IPMI_IP")
+    IPMI_USER = os.getenv("IPMI_USER")
+    IMPI_PASS = os.getenv("IMPI_PASS")
---
+++ handlers/start.py
@@ -0,0 +1,19 @@
+from aiogram import Router, types, F
+from aiogram.filters import Command
+from keyboard.inline import main_menu, status
+
+router = Router()
+
+status = "Here will be current status"
+
+@router.message(Command("start"))
+async def menu(message: types.Message):
+    await message.answer("Basic menu", reply_markup=main_menu())
+
+@router.callback_query(F.data="status")
+async def show_status(callback: types.CallbackQuery):
+    await messahe.edit_text(f"Status: {status}", reply_markup=status())
+
+@router.callback_query(F.data="refresh")
+async def refresh(callback: types.CallbackQuery):
+    await message.edit_text(f"Status: {status}", reply_markup=status())
